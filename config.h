ed249e91bea1b90bfd1db3de6aa06b31775e5587
Ryan Lam <mrsssprite@gmail.com>
2026-04-03 16:01:08 +0800

    refactor: replace font with LXGW WenKai

--- config.h
+++ config.h
@@ -13,8 +13,8 @@ static       int smartgaps          = 1;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "MonaspiceRn NF:size=16" };
-static const char dmenufont[]       = "MonaspiceRn NF:size=16";
+static const char *fonts[]          = { "LXGWWenKai Nerd Font:size=13" };
+static const char dmenufont[]       = "LXGWWenKai Nerd Font:size=13";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
